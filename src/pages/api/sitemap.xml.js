9bf2a7e11445cd5d688fae1ecc337759f37e16a5
linhaverde <linhaverdeveiculos@gmail.com>
2025-05-28 17:00:46 -0300

    sitemap2

--- src/pages/api/sitemap.xml.js
+++ src/pages/api/sitemap.xml.js
@@ -1,37 +1,43 @@
-export default function handler(req, res) {
+eexport default function handler(req, res) {
+  // Definir o Content-Type como application/xml
   res.setHeader('Content-Type', 'application/xml');
-  res.write('<?xml version="1.0" encoding="UTF-8"?>');
-  res.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
-  res.write('<url>');
-  res.write('<loc>https://espacoautomotivo.vercel.app/</loc>');
-  res.write('<lastmod>2025-05-28</lastmod>');
-  res.write('<changefreq>weekly</changefreq>');
-  res.write('<priority>1.0</priority>');
-  res.write('</url>');
-  res.write('<url>');
-  res.write('<loc>https://espacoautomotivo.vercel.app/servicos</loc>');
-  res.write('<lastmod>2025-05-28</lastmod>');
-  res.write('<changefreq>monthly</changefreq>');
-  res.write('<priority>0.8</priority>');
-  res.write('</url>');
-  res.write('<url>');
-  res.write('<loc>https://espacoautomotivo.vercel.app/produtos</loc>');
-  res.write('<lastmod>2025-05-28</lastmod>');
-  res.write('<changefreq>monthly</changefreq>');
-  res.write('<priority>0.8</priority>');
-  res.write('</url>');
-  res.write('<url>');
-  res.write('<loc>https://espacoautomotivo.vercel.app/sobre</loc>');
-  res.write('<lastmod>2025-05-28</lastmod>');
-  res.write('<changefreq>monthly</changefreq>');
-  res.write('<priority>0.7</priority>');
-  res.write('</url>');
-  res.write('<url>');
-  res.write('<loc>https://espacoautomotivo.vercel.app/contato</loc>');
-  res.write('<lastmod>2025-05-28</lastmod>');
-  res.write('<changefreq>monthly</changefreq>');
-  res.write('<priority>0.7</priority>');
-  res.write('</url>');
-  res.write('</urlset>');
-  res.end();
+  
+  // Criar o conteúdo do sitemap
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://espacoautomotivo.vercel.app/</loc>
+    <lastmod>2025-05-28</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://espacoautomotivo.vercel.app/servicos</loc>
+    <lastmod>2025-05-28</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://espacoautomotivo.vercel.app/produtos</loc>
+    <lastmod>2025-05-28</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://espacoautomotivo.vercel.app/sobre</loc>
+    <lastmod>2025-05-28</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://espacoautomotivo.vercel.app/contato</loc>
+    <lastmod>2025-05-28</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+</urlset>`;
+
+  // Enviar a resposta
+  res.status(200 ).send(sitemap);
 }
+
