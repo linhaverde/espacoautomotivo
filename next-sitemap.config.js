0b8dbcdec79fff5485de63083e540de8cb22bf64
linhaverde <linhaverdeveiculos@gmail.com>
2025-05-29 17:49:55 -0300

    sitemap do gemini

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://espacoautomotivo.vercel.app',
-  generateRobotsTxt: true,   // também gera o robots.txt automático
-  sitemapSize: 5000,
-  changefreq: 'weekly',
-  priority: 0.7,
-  exclude: ['/admin/*', '/temp/*', '/private/*'], // rotas que quer excluir do sitemap
-}
